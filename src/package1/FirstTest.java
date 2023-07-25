939f06ccfb2a3658b66b2795291790f1c7c6c99d
Krishna Reddy <Krishna Reddy@LAPTOP-BNR2PJK2>
2023-07-25 12:27:03 +0530

    first commit....

---
+++ src/package1/FirstTest.java
@@ -0,0 +1,27 @@
+package package1;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+
+			
+				int a[]= {2,3,8,5,85,63};
+				System.out.println("print even");
+				for(int i=0;i<a.length;i++)
+				if (a[i]%2==0)
+				{
+					System.out.println(" "+a[i]);
+				}
+				
+				
+				System.out.println("print odd");
+				for(int i=0;i<a.length;i++)
+				if (a[i]%2!=0)
+				{
+					System.out.println(" "+a[i]);
+				}
+			}
+
+		}
+
+
